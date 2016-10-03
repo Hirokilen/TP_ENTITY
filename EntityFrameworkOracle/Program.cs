e55512005a283cc177e28de995912fd18eae1ba8
Hirokilen <dylanraimon@gmail.com>
2016-10-03 11:27:18 +0200

    les premiers test

--- EntityFrameworkOracle/Program.cs
+++ EntityFrameworkOracle/Program.cs
@@ -10,6 +10,31 @@ namespace EntityFrameworkOracle
     {
         static void Main(string[] args)
         {
+            using (OracleEntities oracleContexte = new OracleEntities())
+            {
+                var requeteEmployes = from EMPLOYE in oracleContexte.EMPLOYEs
+                                      select EMPLOYE;
+                var lesEmployes = requeteEmployes.ToList();
+
+                foreach (var unEmploye in lesEmployes)
+                {
+                    Console.WriteLine(unEmploye.NUMEMP + " - " + unEmploye.NOMEMP);
+                }
+
+                Console.WriteLine("---------------------------------------------------------");
+                var unCodeProjet = "PR1";
+                var requeteEmployesProjet = from EMPLOYE in oracleContexte.EMPLOYEs
+                                            where EMPLOYE.CODEPROJET.TrimEnd() == unCodeProjet
+                                            select EMPLOYE;
+                lesEmployes = requeteEmployesProjet.ToList();
+
+                foreach (var unEmploye in lesEmployes)
+                {
+                    Console.WriteLine(unEmploye.NUMEMP + " - " + unEmploye.NOMEMP);
+                }
+
+            }
+            Console.ReadLine();
         }
     }
 }
